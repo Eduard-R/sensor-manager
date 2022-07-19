82c2e8a0914ef5c295418b8c3c42eafedfa48c48
Eduard Richert <erichert@conet.de>
2022-07-19 11:34:26 +0200

    unit 7

--- webapp/controller/Sensors.controller.js
+++ webapp/controller/Sensors.controller.js
@@ -2,12 +2,15 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/IconColor",
     "sap/m/MessageToast",
-    "sap/ui/model/Filter"
-], function (Controller, IconColor, MessageToast, Filter) {
+    "sap/ui/model/Filter",
+    "sap/ui/core/Fragment"
+], function (Controller, IconColor, MessageToast, Filter, Fragment) {
     "use strict";
 
     return Controller.extend("keepcool.SensorManager.controller.Sensors", {
         onInit: function () {
+            this._aCustomerFilters = [];
+            this._aStatusFilters = [];
             this.getSensorModel().dataLoaded().then(function () {
                 MessageToast.show(this.getOwnerComponent().getModel("i18n").getResourceBundle().getText("msgSensorDataLoaded"), { closeOnBrowserNavigation: false });
             }.bind(this));
@@ -28,9 +31,6 @@ sap.ui.define([
             }
         },
         onSensorSelect: function (oEvent) {
-            this._aCustomerFilters = [];
-            this._aStatusFilters = [];
-
             var oBinding = this.getView().byId("sensorsList").getBinding("items"),
                 sKey = oEvent.getParameter("key"),
                 oThreshold = this.getSensorModel().getProperty("/threshold");
@@ -45,7 +45,38 @@ sap.ui.define([
                 this._aStatusFilters = [];
             }
 
-            oBinding.filter(this._aStatusFilters);
+            oBinding.filter(this._aStatusFilters.concat(this._aCustomerFilters));
+        },
+        onCustomerSelect: function () {
+            if (!this._pDialog) {
+                this._pDialog = Fragment.load({
+                    type: "XML",
+                    name: "keepcool.sensormanager.view.CustomerSelectDialog",
+                    controller: this
+                }).then(function (oDialog) {
+                    oDialog.setModel(this.getSensorModel(), "sensorModel");
+                    oDialog.setModel(this.getView().getModel("i18n"), "i18n");
+                    oDialog.setMultiSelect(true);
+                    return oDialog;
+                }.bind(this));
+            }
+            this._pDialog.then(function (oDialog) {
+                oDialog.open();
+            });
+        },
+        onCustomerSelectChange: function (oEvent) {
+            var sValue = oEvent.getParameter("value");
+            var oFilter = new Filter("name", "Contains", sValue);
+            var oBinding = oEvent.getSource().getBinding("items");
+            oBinding.filter([oFilter]);
+        },
+        onCustomerSelectConfirm: function (oEvent) {
+            var aSelectedItems = oEvent.getParameter("selectedItems");
+            var oBinding = this.getView().byId("sensorsList").getBinding("items");
+            this._aCustomerFilters = aSelectedItems.map(function (oItem) {
+                return new Filter("customer", "EQ", oItem.getTitle());
+            });
+            oBinding.filter(this._aCustomerFilters.concat(this._aStatusFilters));
         }
     });
 });
